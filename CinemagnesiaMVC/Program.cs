01dc245502581fb083a58da8dae5bb161b184291
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-04-13 14:14:15 +0300

    Authorization added for Admin Area

--- CinemagnesiaMVC/Program.cs
+++ CinemagnesiaMVC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -29,7 +32,8 @@ app.UseEndpoints(endpoints =>
      name: "Admin",
      areaName: "Admin",
      pattern: "Admin/{controller=Dashboards}/{action=Dashboard_1}"
- );
+ ).RequireAuthorization("Admin");
+
     endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}");
