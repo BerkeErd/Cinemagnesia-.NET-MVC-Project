23f3d7ab8396a73f8ed7814c87f2759ffd107622
Leucosiann <64666918+Leucosiann@users.noreply.github.com>
2023-04-17 13:46:31 +0300

    Generic Repostory added

---
+++ Cinemagnesia.Infrastructure.DataAccess/Repostories/IRepostory.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataAccess.Repostories
+{
+    public interface IRepostory <TEntity> where TEntity : BaseEntity, new()
+    {
+        Task<TEntity> CreateAsync (TEntity entity);
+        Task<TEntity> UpdateAsync (TEntity entity);
+        Task<TEntity> DeleteAsync (Guid id);  
+        Task<TEntity> GetByIdAsync (Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
