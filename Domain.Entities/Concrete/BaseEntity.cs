b502df23c4bdb91df9e2ab1f85f07811d7834118
bernabayraktar <bernabayraktar24@gmail.com>
2023-04-17 13:07:28 +0300

    Edited Entity classes.

---
+++ Domain.Entities/Concrete/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Concrete
+{
+    public class BaseEntity : IEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
--- Domain.Entities/Concrete/Movie.cs
+++ Domain.Entities/Concrete/Movie.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Domain.Entities.Constants;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities.Concrete
 {
-    public class Movie
+    public class Movie : BaseEntity
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PosterPath { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [Range(0, 10f)]
+        public float ImdbRating { get; set; }
+        [Range(0, 10f)]
+        public float CinemagAvgScore { get; set; }
+        public Status Status { get; set; }
+        public string TrailerUrl { get; set; }
+        public ICollection<Director> Directors { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+        public ICollection<CastMembers> CastMembers { get; set; }
+        public ICollection<MovieComment> MovieComments { get; set; }
+        public ICollection<User> WatchedUsers { get; set; }
+        public ICollection<User> RatedUsers { get; set; }
+        public ICollection<User> WantToWatchUsers { get; set; }
+
     }
 }
