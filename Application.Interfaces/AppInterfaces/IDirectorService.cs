2beb3e637c359840f5e08cb91affe79c9f578d3e
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-04-18 19:13:50 +0300

    Director Service has been added

---
+++ Application.Interfaces/AppInterfaces/IDirectorService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.AppInterfaces
+{
+    public interface IDirectorService
+    {
+        IEnumerable<Director> GetAllGenres();
+        void AddDirector(Director genre);
+        void RemoveDirector(string id);
+        void UpdateDirector(string id, Director genre);
+        Director GetById(string id);
+    }
+}
---
+++ Application.Services/DirectorService.cs
@@ -0,0 +1,45 @@
+﻿using Application.Interfaces.AppInterfaces;
+using Domain.Entities.Concrete;
+using Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class DirectorService : IDirectorService
+    {
+        private readonly IDirectorRepository _DirectorRepository;
+
+        public DirectorService(IDirectorRepository DirectorRepository)
+        {
+            _DirectorRepository = DirectorRepository;
+        }
+        public void AddDirector(Director director)
+        {
+            _DirectorRepository.CreateAsync(director).Wait();
+        }
+
+        public IEnumerable<Director> GetAllGenres()
+        {
+          return _DirectorRepository.GetAllAsync().Result;
+        }
+
+        public Director GetById(string id)
+        {
+            return _DirectorRepository.GetByIdAsync(id).Result;
+        }
+
+        public void RemoveDirector(string id)
+        {
+            _DirectorRepository.DeleteAsync(id).Wait();
+        }
+
+        public void UpdateDirector(string id, Director director)
+        {
+            _DirectorRepository.UpdateAsync(id, director).Wait();
+        }
+    }
+}
