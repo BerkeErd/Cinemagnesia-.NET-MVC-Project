e1e27f3e14b5df86ae0353db30da5af4ef1e4c7d
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-04-18 14:02:26 +0300

    Register Message is updated

--- Cinemagnesia.Presentation/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Cinemagnesia.Presentation/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -146,9 +146,18 @@ namespace Cinemagnesia.Presentation.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "LÜTFEN E-POSTANIZI ONAYLAYIN",
-    $"Merhaba,<br><br>Bu bir onay e-postasıdır. Hesabınızın doğrulanması için aşağıdaki linke tıklayarak e-postanızı onaylayabilirsiniz:<br><br><a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Onaylama Linki</a><br><br>Bu e-posta, sizin tarafınızdan istenmiş ve hizmetimizle ilgili bilgilendirmeler almak istediğinizi belirtmiş olduğunuz için gönderilmiştir. Eğer bu e-postayı isteğiniz dışında aldıysanız, lütfen dikkate almayınız ve bize bildirin.<br><br>Teşekkür ederiz.<br>");
-
+                    await _emailSender.SendEmailAsync(Input.Email, "Hesabınızın Onaylanması İçin Lütfen E-Postanızı Onaylayın",
+    @"<html>
+        <body style='font-family: Arial, sans-serif; font-size: 14px; color: #000000;'>
+            <p style='color: #e50914;'>Sayın Film Sever,</p>
+            <p>Cinemagnesia'ya hoş geldiniz! Hesabınızın güvenliği için, e-posta adresinizi doğrulamanız gerekiyor.</p>
+            <p>Hesabınızı aktif hale getirmek için lütfen aşağıdaki linke tıklayarak e-postanızı onaylayınız:</p>
+            <a href='" + HtmlEncoder.Default.Encode(callbackUrl) + @"' style='background-color: #e50914; border: none; color: #ffffff; padding: 10px 15px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin-bottom: 15px;'>Onaylama Linki</a>
+            <p>Bu e-posta sizin tarafınızdan istenmiş ve hizmetimizle ilgili bilgilendirmeler almak istediğinizi belirttiğiniz için gönderilmiştir. Eğer bu e-postayı isteğiniz dışında aldıysanız, lütfen dikkate almayınız ve bize bildirin.</p>
+            <p>İyi seyirler!</p>
+            <p style='color: #e50914; font-weight: bold;'>Cinemagnesia Ekibi</p>
+        </body>
+    </html>");
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
