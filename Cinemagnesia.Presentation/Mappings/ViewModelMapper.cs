1a47739da430d1833009c381e37592b2fa2ca542
emirhanaltuntas <altuntasemirhan99@gmail.com>
2023-04-27 16:16:55 +0300

    add mapper file

---
+++ Cinemagnesia.Presentation/Mappings/ViewModelMapper.cs
@@ -0,0 +1,27 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Cinemagnesia.Presentation.Areas.Admin.Models;
+using Cinemagnesia.Presentation.Models;
+
+namespace Cinemagnesia.Presentation.Mappings
+{
+    public class ViewModelMapper : Profile
+    {
+        public ViewModelMapper()
+        {
+            CreateMap<GenreDto, GenreViewModel>();
+
+            CreateMap<GenreViewModel, GenreDto>();
+
+            CreateMap<AddGenreViewModel, GenreDto>();
+
+            CreateMap<GenreDto, AddGenreViewModel>();
+
+            CreateMap<AddProductorRequestViewModel, AddProductorRequestDto>();
+            CreateMap<ProductorRequestDto, UserProductorRequestViewModel>();
+
+            CreateMap<ProductorRequestDto, AdminProductorRequestViewModel>();
+            CreateMap<AdminProductorRequestViewModel, ProductorRequestDto>();
+        }
+    }
+}
