ac5f77d01074797e1c575b5f3c45323f443a9996
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-04-28 15:17:03 +0300

    Unfinished version of the addmovie page for productor is added

--- Cinemagnesia.Presentation/Controllers/ProductorPageController.cs
+++ Cinemagnesia.Presentation/Controllers/ProductorPageController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Dtos;
+using Application.Interfaces.AppInterfaces;
+using AutoMapper;
+using Cinemagnesia.Presentation.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cinemagnesia.Presentation.Controllers
 {
     public class ProductorPageController : Controller
     {
+        private IGenreService _genreService;
+        private IMapper _mapper;
+
+        public ProductorPageController(IGenreService genreService, IMapper mapper)
+        {
+            _genreService = genreService;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,7 +24,10 @@ namespace Cinemagnesia.Presentation.Controllers
 
         public IActionResult AddMovieView()
         {
-            return View();
+            List<GenreDto> genredtos = _genreService.GetAllGenres();
+            List<GenreViewModel> genreViewModels = _mapper.Map<List<GenreViewModel>>(genredtos)  ;
+
+            return View(genreViewModels);
         }
 
         public IActionResult ListMovieView()
