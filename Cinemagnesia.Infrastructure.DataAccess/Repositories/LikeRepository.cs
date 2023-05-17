b57ca054ca8072166bb471caa6cad6d4750eac5b
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-05-17 14:03:04 +0300

    Added LikeRepository and LikeService with their Interfaces.

---
+++ Cinemagnesia.Infrastructure.DataAccess/Repositories/LikeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Cinemagnesia.Infrastructure.DataAccess.DbContext;
+using Domain.Entities.Concrete;
+using Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataAccess.Repositories
+{
+    public class LikeRepository : BaseRepository<Like>, ILikeRepository
+    {
+        public LikeRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            
+        }
+    }
+}
